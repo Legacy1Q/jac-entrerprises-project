44d6012b31dc5cb2c2ae167e05169721871dd54a
rheeze-dev <ggybzz2297@gmail.com>
2023-08-25 20:55:08 -0400

    Generated spring boot

---
+++ jac-enterprise/src/main/java/com/wcci/jacenterprise/JacEnterpriseApplication.java
@@ -0,0 +1,13 @@
+package com.wcci.jacenterprise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacEnterpriseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JacEnterpriseApplication.class, args);
+	}
+
+}
