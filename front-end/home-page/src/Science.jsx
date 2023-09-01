f4faa7f153a552b1ec7aa610f52db8a15e9584a1
Legacy1Q <mdsims2@outlook.com>
2023-08-31 23:04:52 -0400

    finished sample/simple learn page (science page)

--- front-end/home-page/src/Science.jsx
+++ front-end/home-page/src/Science.jsx
@@ -5,7 +5,52 @@ function Science() {
   return (
     <div className='science'>
       <div className="science__content">
-        <h1>Science Page</h1>
+        <h1>Plants</h1>
+
+        <div className="science__row">
+          <img src="./images/bellsprout.png" alt="plant" />
+        </div>
+        <div className="science__row">
+          <h1>6 Parts of the Plant</h1>
+          <h3>Plants are often referred to as ‘green lungs’ for the crucial role they play in generating oxygen responsible for the survival of every other creature on Earth. There are several parts of a plant for kids to learn and each of them performs a different function that can be explained to kids in detail. Different plants require different soil, temperature, and climate conditions to survive and thrive. 
+
+Oxygen is crucial for the survival of all living organisms on the planet. They are an important source of food and nutrition for Herbivore (plant-eating) and Omnivore (animal- and plant-eating) animals. Several plants are medicinal in nature and have useful healing properties.</h3>
+
+          <div className="science__row">
+            <h1>Roots</h1>
+            <img src="./images/roots.jpg" alt="roots" />
+            <h3>Plants are often referred to as ‘green lungs’ for the crucial role they play in generating oxygen responsible for the survival of every other creature on Earth. There are several parts of a plant for kids to learn and each of them performs a different function that can be explained to kids in detail. Different plants require different soil, temperature, and climate conditions to survive and thrive. 
+
+Oxygen is crucial for the survival of all living organisms on the planet. They are an important source of food and nutrition for Herbivore (plant-eating) and Omnivore (animal- and plant-eating) animals. Several plants are medicinal in nature and have useful healing properties.</h3>
+          </div>
+          <div className="science__row">
+            <h1>Stem</h1>
+            <img src="./images/stem.jpg" alt="stem" />
+            <h3>Stems are the part of the plant that are seen above the soil. They help support the plant structure and hold them upright. They act as an important transport system, carrying nutrients, sugar, and water from the soil to the rest of the plant. Xylem cells in a stem carry water, whereas phloem cells in a stem carry food. Some flowering plants have a soft and bendy stem, while others such as trees, have stems in the form of a trunk, which is strong and woody.
+
+Stems form the shoot system of a plant. They bear the leaves, fruits, and flowers and are seen growing above the soil. The part of the stem from the leaves that arise is called nodes. The part in between the nodes is called internodes. </h3>
+          </div>
+          <div className="science__row">
+            <h1>Leaves</h1>
+            <img src="./images/leaf.png" alt="leaves" />
+            <h3>Leaves are where food is made through a process called photosynthesis. The green-colored substance called chlorophyll in the leaf absorbs sunlight, water, and carbon dioxide to prepare food and oxygen for the plant. This food is created in the form of glucose or sugar. Leaves have a waxy coating called the cuticle, which protects the leaves. The three parts of a leaf are called the petiole, the leaf base, and the lamina.</h3>
+          </div>
+          <div className="science__row">
+            <h1>Flowers</h1>
+            <img src="./images/flower.jpg" alt="flowers" />
+            <h3>Flowers are the reproductive part of a plant. They are the most colorful and attractive part of a plant. They have a sweet fragrance and are the most beautiful part of a plant. Flowers are the reproductive part of a plant. They are the most colorful and attractive part of a plant. They have a sweet fragrance and are the most beautiful part of a plant. Flowers are the reproductive part of a plant. They are the most colorful and attractive part of a plant. They have a sweet fragrance and are the most beautiful part of a plant. </h3>
+          </div>
+          <div className="science__row">
+            <h1>Fruits</h1>
+            <img src="./images/fruit.jpg" alt="fruits" />
+            <h3>Fruits are the part of a plant that is produced from a flower. They are the part of a plant that contains seeds. They are the part of a plant that is produced from a flower. They are the part of a plant that contains seeds. They are the part of a plant that is produced from a flower. They are the part of a plant that contains seeds. They are the part of a plant that is produced from a flower. They are the part of a plant that contains seeds. </h3>
+          </div>
+          <div className='science__row'>
+            <h1>Seeds</h1>
+            <img src="./images/seed.jpg" alt="seeds" />
+            <h3>Seeds contain the embryo or the plant material required to grow into another plant. They are present inside the fruit till they are germinated. Seeds are protected by a seed coat and have one or two cotyledons. Cotyledons act as the main source of food for a baby plant until they can produce their own food through photosynthesis. They are also the first embryonic leaves of the plant.   </h3>
+          </div>
+        </div>
       </div>
     </div>
   )
